d378f7891bef957ae29148b88432534f58d810eb
eisemsi2 <96539950+eisemsi2@users.noreply.github.com>
2024-03-08 22:27:08 +0000

    changed name and photo

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Prajjwal Kumar",
+  title: "Software Developer",
+  email: "prajjwalk255@gmail.com",
+  gitHub: "https://github.com/eisemsi2",
 };
 
 const primaryColor = "#4E567E";
